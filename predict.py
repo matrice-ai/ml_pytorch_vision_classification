3b5c1e7d98f4b0d6f4ef53b9695ebe0fa7e83380
root <mohned.moneam@matrice.ai>
2024-02-29 06:55:43 +0000

    update predict.py

--- predict.py
+++ predict.py
@@ -7,44 +7,43 @@ import numpy as np
 import torch.nn.functional as F
 
 def load_model(actionTracker):
-    
-
     actionTracker.download_model("model.pt")
     model = torch.load('model.pt', map_location='cpu')
 
-    if torch.cuda.is_available():
-        device = torch.device("cuda")
-        model = model.to(device)
-        
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Device is :{device}")
+    model = model.to(device)
+
     model.eval()
     return model
 
-def predict(model,image_bytes):
-        # Convert image bytes to PIL Image
-        image = Image.open(BytesIO(image_bytes)).convert('RGB')
+def predict(model, image_bytes):
+    # Convert image bytes to PIL Image
+    image = Image.open(BytesIO(image_bytes)).convert('RGB')
 
-        # Preprocess the image
-        transform = transforms.Compose([
-            transforms.Resize((224, 224)),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ])
-        input_tensor = transform(image).unsqueeze(0)  # Add batch dimension
-        
-        if torch.cuda.is_available():
-            device = torch.device("cuda")
-            input_tensor.to(device)
+    # Preprocess the image
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    input_tensor = transform(image).unsqueeze(0)  # Add batch dimension
+
+    # Move input tensor to the same device as model parameters
+    device = next(model.parameters()).device
+    input_tensor = input_tensor.to(device)
+
+    # Perform inference
+    with torch.no_grad():
+        output = model(input_tensor)
+
+    # Apply softmax to get probabilities
+    probabilities = F.softmax(output, dim=1)
+
+    # Get the confidence for the predicted class
+    predicted_class = torch.argmax(output, 1).item()
+    confidence = round(probabilities[0, predicted_class].item(), 2)
+
+    return {"category": str(predicted_class), "confidence": confidence}
             
-        # Perform inference
-        with torch.no_grad():
-            output = model(input_tensor)
 
-    
-        # Apply softmax to get probabilities
-        probabilities = F.softmax(output, dim=1)
-    
-        # Get the confidence for the predicted class
-        predicted_class = torch.argmax(output, 1).item()
-        confidence = round(probabilities[0, predicted_class].item(), 2)
-
-        return {"category": str(predicted_class), "confidence": confidence}
