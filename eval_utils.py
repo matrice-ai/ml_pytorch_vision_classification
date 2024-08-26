3266d3cd9ab5ed3abbe94606a2351a96f780d398
MohnedMoneam0 <147365459+MohnedMoneam0@users.noreply.github.com>
2024-08-26 12:41:33 +0300

    Update eval_utils.py
    Signed-off-by: MohnedMoneam0 <147365459+MohnedMoneam0@users.noreply.github.com>

--- eval_utils.py
+++ eval_utils.py
@@ -118,7 +118,7 @@ def get_openvino_inference_results(loader, model):
 
     return all_predictions, all_outputs, all_targets
             
-def get_torchscript_inference_results(loader, model):
+def get_pytorch_inference_results(loader, model):
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     all_outputs = []
     all_targets = []
