1ea177ddbab675698a84e90900bab6c1fac85143
MohnedMoneam0 <147365459+MohnedMoneam0@users.noreply.github.com>
2024-09-09 07:28:39 +0300

    Update predict.py
    Signed-off-by: MohnedMoneam0 <147365459+MohnedMoneam0@users.noreply.github.com>

--- export_formats/tensorrt/predict.py
+++ export_formats/tensorrt/predict.py
@@ -8,6 +8,15 @@ import torch
 from torchvision import transforms
 import torch.nn.functional as F
 
+def allocate_buffers(engine):
+    # Determine dimensions and create page-locked memory buffers (i.e., pinned memory)
+    h_input = cuda.pagelocked_empty(trt.volume(engine.get_tensor_shape("images")), dtype=np.float32)
+    h_output = cuda.pagelocked_empty(trt.volume(engine.get_tensor_shape("output")), dtype=np.float32)
+    d_input = cuda.mem_alloc(h_input.nbytes)
+    d_output = cuda.mem_alloc(h_output.nbytes)
+    stream = cuda.Stream()
+    return (h_input, d_input, h_output, d_output, stream)
+
 def load_model(actionTracker):
     
     actionTracker.download_model("model.engine",model_type="exported")
@@ -17,16 +26,32 @@ def load_model(actionTracker):
     with open("model.engine", 'rb') as f:
         engine_data = f.read()
         engine = runtime.deserialize_cuda_engine(engine_data)
-
+    if not engine:
+      print(f"Failed to load the engine: {engine}")
     # Create a TensorRT context
     context = engine.create_execution_context()
+    buffers = allocate_buffers(engine)
 
-    return (engine, context)
+    return (engine, context, buffers)
 
+def do_inference(engine, context, h_input, d_input, h_output, d_output, stream):
+    # Transfer input data to device
+    cuda.memcpy_htod_async(d_input, h_input, stream)
+    # Run inference
+    context.execute_v2(bindings=[int(d_input), int(d_output)])
+    # Transfer predictions back from device
+    cuda.memcpy_dtoh_async(h_output, d_output, stream)
+    # Synchronize the stream
+    stream.synchronize()
+
+    return h_output
 
 def predict(model, image_bytes):
 
-    engine, context = model[0] ,model[1]
+    engine = model[0]
+    context = model[1]
+    buffers = model[2]
+    h_input, d_input, h_output, d_output, stream = buffers
     # Convert image bytes to PIL Image
     image = Image.open(BytesIO(image_bytes)).convert('RGB')
 
@@ -36,27 +61,12 @@ def predict(model, image_bytes):
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
-    input_tensor = transform(image).unsqueeze(0)  # Add batch dimension
+    image = transform(image).unsqueeze(0).numpy().astype(np.float32)  # Add batch dimension
+    h_input = np.ravel(image)
 
-    # Allocate GPU memory for input and output buffers
-    input_size = trt.volume(engine.get_tensor_shape("images"))
-    output_size = trt.volume(engine.get_tensor_shape("output"))
-    
-    input_buffer = cuda.pagelocked_empty(input_size, dtype=np.float32)
-    output_buffer = cuda.pagelocked_empty(output_size, dtype=np.float32)
-    
-    # Copy input data to GPU
-    np.copyto(input_buffer, input_tensor.numpy().ravel())
-    cuda.memcpy_htod(cuda.mem_alloc(input_buffer.nbytes), input_buffer)
-    
-    # Run inference
-    context.execute_v2(bindings=[cuda.mem_alloc(input_buffer.nbytes), cuda.mem_alloc(output_buffer.nbytes)])
-    
-    # Copy output data from GPU
-    cuda.memcpy_dtoh(output_buffer, cuda.mem_alloc(output_buffer.nbytes))
-
+    output = do_inference(engine, context, h_input, d_input, h_output, d_output, stream)
     # Process the results
-    probabilities = F.softmax(torch.from_numpy(output_buffer).to("cpu"), dim=0)
+    probabilities = F.softmax(torch.tensor(output).to("cpu"), dim=0)
     predicted_class = torch.argmax(probabilities).item()
     confidence = round(probabilities[predicted_class].item(), 2)
 
